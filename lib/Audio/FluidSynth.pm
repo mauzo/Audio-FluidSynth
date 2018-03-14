dfc6ef27633dd5ef76218e4815332ff144e31a62
Ben Morrow <ben@morrow.me.uk>
2018-03-14 12:29:18 +0000

    Metadata.

--- lib/Audio/FluidSynth.pm
+++ lib/Audio/FluidSynth.pm
@@ -1,13 +1,34 @@
 package Audio::FluidSynth;
 
+=head1 NAME
+
+Audio::FluidSynth - Bind the fluidsynth MIDI library
+
+=cut
+
 use 5.010;
 use warnings;
 use strict;
 
-our $VERSION = "0";
+our $VERSION = "1";
 
 use XSLoader;
 
 XSLoader::load __PACKAGE__, $VERSION;
 
 1;
+
+=head1 BUGS
+
+Please report to <L<bug-Audio-FluidSynth@rt.cpan.org>>.
+
+=head1 AUTHOR
+
+Ben Morrow <ben@morrow.me.uk>
+
+=head1 COPYRIGHT
+
+Copyright 2014 Ben Morrow.
+
+Released under the 2-clause BSD licence.
+
